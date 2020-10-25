0e88b930af6877b13f9149ebe9b2b8961e7356bf
abimacarmes <a.mackenzie.armes@gmail.com>
2020-10-25 14:02:12 -0400

    added the nutrition API and finalized design

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function grocerySubmit(){
         for(let i=0;i<STORE.savedRecipes.length;i++){      
             STORE.savedRecipes[i].extendedIngredients.forEach(function(ingrs){
                 if(ingrs.original in groceryObject){
-                    groceryObject[ingrs.name][0] += ingrs.amount
+                    groceryObject[ingrs.name][0] += ingrs.measures.metric.amount
                 }
                 else{
-                    groceryObject[ingrs.name] = [ingrs.amount,ingrs.measures.metric.unitShort];
+                    groceryObject[ingrs.name] = [ingrs.measures.metric.amount,ingrs.measures.metric.unitShort];
                 }
             })     
         }
@@ -30,14 +30,19 @@ function grocerySubmit(){
 }
 
 function renderGroceryList(groceryObject){
-    let groceryListHTML = "<h3>Grocery List</h3>";
+    let groceryListHTML = "<h4>Grocery List:</h4>";
 
     let groceryArray = Object.entries(groceryObject);
     console.log(groceryArray)
 
+    let currIngredientStr = "";
+    let currCalories = 0;
+
     for(let i=0;i<groceryArray.length;i++){
+        currIngredientStr = `${groceryArray[i][0]} - ${groceryArray[i][1][0]} ${groceryArray[i][1][1]}`
+
         groceryListHTML += `<ul>
-            <li>${groceryArray[i][0]} - ${groceryArray[i][1][0]} ${groceryArray[i][1][1]}</li>
+            <li>${currIngredientStr}</li>
         </ul>`
     }
     
@@ -51,30 +56,13 @@ function renderGroceryList(groceryObject){
 }
 
 function endGroceryListButton(){
-    $('.groceryList').on('submit','removeGrocery',event=>{
+    $('.groceryList').on('submit','.removeGrocery',event=>{
         event.preventDefault();
 
         $('.groceryList').html("");
         renderSave();
         $('.content').toggleClass('hidden');
     })
-}
-
-function getNutrition(ingredient){
-    let nutritionURL =`https://api.edamam.com/api/nutrition-data?app_id=a97ec4a6&app_key=b504a6ebf4ca24cb69019c2f626e3734&ingr=${ingredient}`
-    
-    fetch(nutritionURL)
-    .then(response =>{
-        if(response.ok){
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })  
-    .then(responseJson => {
-        console.log(responseJson);
-        return responseJson;
-    })
-    .catch(error=> alert(error.message));
 }
 
 function buttonSubmit(){
@@ -101,35 +89,74 @@ function getResults(){
     .then(responseJson => {
         console.log(responseJson);
         STORE.currRecipe = responseJson.recipes[0];
-        renderRecipe(responseJson.recipes[0]);
+        generateRecipeHTML(responseJson.recipes[0]);
     })
     .catch(error=> alert(error.message));
 }
 
-function renderRecipe(recipeObject){
+function generateRecipeHTML(recipeObject){
+    console.log(recipeObject)
     //Grab the recipe object and get the correct input variables
     const recipeName = recipeObject.title;
     const recipeSrc = recipeObject.image;
 
     const ingredientList = ingredientHTML(recipeObject.extendedIngredients);
-    
+    const instructionList = instructionHTML(recipeObject.analyzedInstructions[0].steps);
+
+    const nutritionArrayInput = getIngredientArray(recipeObject.extendedIngredients);
     const recipeHTML = `
-        <h3>${recipeName}</h3>
-        <img src="${recipeSrc}" width="300">
-        <form class="saveButton">
-            <button type="submit">Save!</button>
-        </form>
-        <h4>Ingredients:</h4>
-        <ul>
-            ${ingredientList}    
-        <li>Ingredient</li>
-        </ul>
-        <h4>Steps:</h4>
-        <ol>
-            ${recipeObject.instructions}
-        </ol>
+            <h3>${recipeName}</h3>
+            <img src="${recipeSrc}">
+            <form class="saveButton">
+                <button type="submit">Save!</button>
+            </form> 
+            <h4>Ingredients:</h4>
+            <ul>
+                ${ingredientList}    
+            </ul>
+            <h4>Steps:</h4>
+            <ol>
+                ${instructionList}
+            </ol>
     `
-    $(".js-recipe").html(recipeHTML);
+    getNutrition(recipeObject.title,nutritionArrayInput,recipeHTML);
+}
+
+function getIngredientArray(ingredientArray){
+    let recipeIngredients = [];
+
+    for(let i=0;i<ingredientArray.length;i++){
+        recipeIngredients.push(ingredientArray[i].name+" "+ingredientArray[i].measures.metric.amount+" "+ingredientArray[i].measures.metric.unitShort);
+    }
+    return recipeIngredients;
+}
+
+function getNutrition(recipeTitle,recipeIngredients,recipeHTML){
+    let totalCalories =0;
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"title": recipeTitle,"ingr": recipeIngredients});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+    };
+
+    fetch("https://api.edamam.com/api/nutrition-details?app_id=a97ec4a6&app_key=b504a6ebf4ca24cb69019c2f626e3734", requestOptions)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+        console.log(responseJson)
+        recipeHTML+=`<h4>Total Calories: ${responseJson.calories} - Servings: ${responseJson.yield}</h4>`
+        $(".js-recipe").html(recipeHTML);
+    })
+    .catch(error => console.log(error.message));
 }
 
 function saveSubmit(){
@@ -142,6 +169,26 @@ function saveSubmit(){
         $('.searchButton').trigger('reset');
         renderSave();
     })
+}
+
+function renderSave(){
+    let savedHTML = "";
+
+    for(let i=0;i<STORE.savedRecipes.length;i++){
+        savedHTML += `
+        <div class="item">
+            <div class="${i}">
+                <h4>${STORE.savedRecipes[i].title}</h4>
+                <form class="deleteButton">
+                    <button type="submit">Remove!</button>
+                </form>
+                <img src="${STORE.savedRecipes[i].image}" width="100">
+            </div>
+        </div>
+        `
+    }
+
+    $('.js-saved').html(savedHTML);
 }
 
 function deleteSubmit(){
@@ -157,23 +204,6 @@ function deleteSubmit(){
     })
 }
 
-function renderSave(){
-    let savedHTML = "";
-
-    for(let i=0;i<STORE.savedRecipes.length;i++){
-        savedHTML += `
-        <div class="${i}">
-            <h4>${STORE.savedRecipes[i].title}</h4>
-            <img src="${STORE.savedRecipes[i].image}" width="100">
-            <form class="deleteButton">
-                <button type="submit">Remove!</button>
-            </form>
-        </div>
-        `
-    }
-
-    $('.js-saved').html(savedHTML);
-}
 function ingredientHTML(ingredientArray){
     let ingredientList = "";
     for(let i =0; i < ingredientArray.length;i++){
@@ -182,6 +212,15 @@ function ingredientHTML(ingredientArray){
             ${currIngredient.measures.metric.amount} ${currIngredient.measures.metric.unitShort}</li>`
     }
     return ingredientList;
+}
+
+function instructionHTML(instructionArray){
+    let instructionList = "";
+    for(let i =0; i < instructionArray.length;i++){
+        let currInstruction = instructionArray[i];
+        instructionList += `<li>${currInstruction.step}</li>`
+    }
+    return instructionList;
 }
 
 $(pageLoaded);
